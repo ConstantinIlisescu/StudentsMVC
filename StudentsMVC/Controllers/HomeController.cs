9c8c7075acf648690e77f84850d42068d786ac9c
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-07 20:23:05 +0100

    Change the HomeControler  ViewBag.Message value

--- StudentsMVC/Controllers/HomeController.cs
+++ StudentsMVC/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace StudentsMVC.Controllers
 {
@@ -22,7 +18,7 @@ namespace StudentsMVC.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Page - Academy of Learning.";
 
             return View();
         }
