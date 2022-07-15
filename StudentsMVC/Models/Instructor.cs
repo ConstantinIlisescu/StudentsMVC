31cfe8284820c566daff5bd8faa9aef3314a8fa3
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-07-15 14:45:04 +0100

    Add Instructor Models

---
+++ StudentsMVC/Models/Instructor.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentsMVC.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
